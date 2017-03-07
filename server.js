4334e63b97c22a754c1201e80e6396e5d2b27e8e
Tyler Bainbridge <tylerbainbridge@Tylers-MacBook-Pro.local>
2017-03-06 20:03:33 -0500

    changed everything

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './dist')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
