8f4217145ae9c6ab58b6e3aaf4b77dda528b53c7
Tyler Bainbridge <tylerbainbridge@Tylers-MacBook-Pro.local>
2017-03-06 20:08:41 -0500

    changed everything

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('listening on port 3000');
 });
